cd8680eaed72923af6a2c4602543f294bf3e33ad
Brandon O'Donoghue <brandon@brandch.com>
2025-06-28 17:22:35 -0600

    core functionality works

--- script.js
+++ script.js
@@ -2,91 +2,63 @@ const currentCalculation = document.querySelector("#current-calculation");
 const sum = document.querySelector("#sum");
 const buttons = document.querySelectorAll("button");
 
-/*
-function operations (operator,a,b) {
-    if (operator === "+") {
-        return a+b;
-    } else if (operator ==="-") {
-        return a-b;
-    } else if (operator === "*") {
-        return a * b;
-    } else if (operator === "/") {
-        return a/b;
-    } else {
-        return null;
-    }
-}
-
-*/
-
-let firstOperand = "0";
-let secondOperand = null;
+let displayValue = "0";
+let firstOperand = null;
+let secondOperand = null; 
 let operator = null;
 
-function multiplication (a,b) {
-    return a*b;
-}
-function division (a,b) {
-    return a/b;
-}
 function addition (a,b) {
     return a+b;
 }
+
 function subtraction (a,b) {
     return a-b;
 }
 
-function operate (operator, a, b) {
-    if (operator === "*") {
-        return multiplication(a,b);
-    } else if (operator ==="/") {
-        return division(a,b);
-    } else if (operator === "+") {
-        return addition (a,b);
+function multiplication (a,b) {
+    return a*b;
+}
+
+function division (a,b) {
+    return a/b;
+}
+
+function operate(operator,a,b) {
+    if (operator === "+") {
+        return addition(a,b)
     } else if (operator === "-") {
-        return subtraction (a,b);
+        return subtraction(a,b)
+    } else if (operator === "*") {
+        return multiplication(a,b)
+    }   else if (operator === "/") {
+        return division(a,b)
     } else {
         return null;
     }
 }
 
-console.log(operate("*", 7, 4));
-console.log(operate("/", 7, 4));
-console.log(operate("+", 7, 4));
-console.log(operate("-", 7, 4));
-console.log(operate("s", 7, 4));
-
-
-displayValue = "0";
-
-sum.textContent = displayValue;
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const value = button.textContent;
-
-        if (["*","/","+","-"].includes(value)) {
-            if (firstOperand === "") {
-                firstOperand = displayValue;
-            }
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const value = btn.textContent;
+        
+        if (["+", "-", "/", "*"].includes(btn.textContent)) {
+            firstOperand = displayValue;
             operator = value;
-            displayValue = "";
+            displayValue = ""
         } else if (value === "=") {
-            secondOperand = displayValue;
-
-            let result = operate(operator, parseFloat(firstOperand), parseFloat(secondOperand));
+            secondOperand = displayValue; 
+            const result = operate(operator, Number(firstOperand), Number(secondOperand));
+            sum.textContent = result;
             displayValue = result.toString();
-            sum.textContent = displayValue;
+
+          }   else {
+            if (displayValue ==="0") {
+                displayValue = value;
+            } else {
+                displayValue += value;
+            }
+            
+            sum.textContent= displayValue;
         }
     })
 })
-
-/* 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const value = button.textContent;
-        displayValue += value;
-        sum.textContent = displayValue;
-    })
-})
-*/
